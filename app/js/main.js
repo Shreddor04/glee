f03ab824b82c27d42b9c06ba8ac5d38530d83334
Shreddor04 <amandurdyevo@gmail.com>
2025-02-22 08:17:28 +0500

    added login page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function(){
-    
+    $('.top-slider-main__inner').slick({
+        dots:true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000
+
+    })
+
+    var mixer = mixitup('.products-items');
+    var mixer = mixitup('.design-items');
+
+
 })
